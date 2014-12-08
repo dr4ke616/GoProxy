b8f5f93e382d70455abf2815d475874f048bfba7
Adam Drakeford <adamdrakeford@gmail.com>
2014-12-08 20:28:48 +0000

    Got basic proxy working

--- webserver.go
+++ webserver.go
@@ -1,31 +1,68 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 )
 
-func RouteHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Write it back to the client.
-		log.Println("[RouteHandler]", r.Method, r.URL, r.UserAgent())
-		fmt.Fprintf(w, "howdy")
-	})
-}
+var target *string
 
 func main() {
-	port := 8080
-	portstring := strconv.Itoa(port)
+	target = flag.String("target", "http://stackoverflow.com", "target URL for reverse proxy")
+	flag.Parse()
+	http.HandleFunc("/", report)
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
 
-	http.Handle("/", RouteHandler())
+func report(w http.ResponseWriter, r *http.Request) {
 
-	log.Println("Starting GO webserver on port", portstring)
-	err := http.ListenAndServe(":"+portstring, nil)
+	uri := *target + r.RequestURI
+
+	fmt.Println(r.Method + ": " + uri)
+
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		fatal(err)
+		fmt.Printf("Body: %v\n", string(body))
+	}
+
+	rr, err := http.NewRequest(r.Method, uri, r.Body)
+	fatal(err)
+	copyHeader(r.Header, &rr.Header)
+
+	// Create a client and query the target
+	var transport http.Transport
+	resp, err := transport.RoundTrip(rr)
+	fatal(err)
+
+	fmt.Printf("Resp-Headers: %v\n", resp.Header)
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fatal(err)
+
+	dH := w.Header()
+	copyHeader(resp.Header, &dH)
+	dH.Add("Requested-Host", rr.Host)
+
+	w.Write(body)
+}
+
+func fatal(err error) {
 	if err != nil {
-		log.Fatal("ListenAndServe error: ", err)
+		log.Fatal(err)
 		os.Exit(1)
+	}
+}
+
+func copyHeader(source http.Header, dest *http.Header) {
+	for n, v := range source {
+		for _, vv := range v {
+			dest.Add(n, vv)
+		}
 	}
 }
