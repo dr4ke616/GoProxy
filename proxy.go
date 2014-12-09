1bc230bac331fb9cb9df17d3bcce21f10e7afb58
Adam Drakeford <adamdrakeford@gmail.com>
2014-12-09 18:25:18 +0000

    Renamed Target struct to Proxy

--- proxy.go
+++ proxy.go
@@ -7,76 +7,76 @@ import (
 	"os"
 )
 
-type Target struct {
-	URL            string
+type Proxy struct {
+	target_url     string
 	listening_port string
 }
 
 func main() {
 
-	target := Target{
-		URL:            "http://stackoverflow.com",
+	proxy := Proxy{
+		target_url:     "http://stackoverflow.com",
 		listening_port: "8080",
 	}
-	err := StartServer(&target)
+	err := StartProxy(&proxy)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 }
 
-func StartServer(t *Target) error {
+func StartProxy(p *Proxy) error {
 
-	http.HandleFunc("/", t.ProxyRequest)
-	log.Println("Started GO proxyserver on port", t.listening_port)
+	http.HandleFunc("/", p.ProxyRequest)
+	log.Println("Started GO proxyserver on port", p.listening_port)
 
-	err := http.ListenAndServe("127.0.0.1:"+t.listening_port, nil)
+	err := http.ListenAndServe("127.0.0.1:"+p.listening_port, nil)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (t *Target) ProxyRequest(w http.ResponseWriter, r *http.Request) {
+func (p *Proxy) ProxyRequest(w http.ResponseWriter, r *http.Request) {
 
-	uri := t.URL + r.RequestURI
+	uri := p.target_url + r.RequestURI
 	log.Println(r.Method + ": " + uri)
 
-	t.MethodHandler(r)
+	p.MethodHandler(r)
 
-	remote_request, err := t.CreateRemoteRequest(r, uri)
+	remote_request, err := p.CreateRemoteRequest(r, uri)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	t.CopyHeader(r.Header, &remote_request.Header)
+	p.CopyHeader(r.Header, &remote_request.Header)
 
-	resp, err := t.Query(remote_request)
+	resp, err := p.Query(remote_request)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
-	body, err := t.ReadBody(resp)
+	body, err := p.ReadBody(resp)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
 	destination_header := w.Header()
-	t.CopyHeader(resp.Header, &destination_header)
+	p.CopyHeader(resp.Header, &destination_header)
 	destination_header.Add("Requested-Host", remote_request.Host)
 	w.Write(body)
 }
 
-func (t *Target) MethodHandler(r *http.Request) {
+func (p *Proxy) MethodHandler(r *http.Request) {
 	if r.Method == "POST" {
 		log.Printf("Method is POST:")
 	}
 }
 
-func (t *Target) CreateRemoteRequest(r *http.Request, uri string) (*http.Request, error) {
+func (p *Proxy) CreateRemoteRequest(r *http.Request, uri string) (*http.Request, error) {
 	rr, err := http.NewRequest(r.Method, uri, r.Body)
 	if err != nil {
 		return nil, err
@@ -84,7 +84,7 @@ func (t *Target) CreateRemoteRequest(r *http.Request, uri string) (*http.Request
 	return rr, nil
 }
 
-func (t *Target) Query(r *http.Request) (*http.Response, error) {
+func (p *Proxy) Query(r *http.Request) (*http.Response, error) {
 	// Create a client and query the target
 	var transport http.Transport
 	resp, err := transport.RoundTrip(r)
@@ -95,7 +95,7 @@ func (t *Target) Query(r *http.Request) (*http.Response, error) {
 	return resp, nil
 }
 
-func (t *Target) ReadBody(r *http.Response) ([]byte, error) {
+func (p *Proxy) ReadBody(r *http.Response) ([]byte, error) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return nil, err
@@ -104,7 +104,7 @@ func (t *Target) ReadBody(r *http.Response) ([]byte, error) {
 	return body, nil
 }
 
-func (t *Target) CopyHeader(source http.Header, dest *http.Header) {
+func (p *Proxy) CopyHeader(source http.Header, dest *http.Header) {
 	for n, v := range source {
 		for _, vv := range v {
 			dest.Add(n, vv)
