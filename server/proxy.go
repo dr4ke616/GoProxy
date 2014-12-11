c49c099898d8e0b2829c4ff007a5df1bec8235ab
Adam Drakeford <adam.drakeford@dedsert.com>
2014-12-11 19:34:23 +0000

    Added beginings of the method handler

--- server/proxy.go
+++ server/proxy.go
@@ -8,20 +8,39 @@ import (
 )
 
 type Proxy struct {
-	ListeningPort string `json:"listening_port"`
-	TargetUrl     string `json:"target_url"`
-	AlterList     []struct {
-		URI        string `json:"uri"`
-		FromMethod string `json:"from_method"`
-		ToMethod   string `json:"to_method"`
-	} `json:"alter_list"`
+	ListeningPort  string `json:"listening_port"`
+	TargetUrl      string `json:"target_url"`
+	RoutingOptions []struct {
+		URI           string   `json:"uri"`
+		FromMethod    string   `json:"from_method"`
+		ToMethod      string   `json:"to_method"`
+		CustomHeaders []string `json:"custom_headers"`
+	} `json:"routing_options"`
+}
+
+type MethodHandler struct {
+	FromMethod, ToMethod string
+	CustomHeaders        []string
+}
+
+type Handler interface {
+	ServeHTTP(http.ResponseWriter, *http.Request)
 }
 
 func StartProxy(p *Proxy) error {
 
+	for _, route := range p.RoutingOptions {
+		log.Println("Adding custom handler for URI", route.URI)
+		handler := MethodHandler{
+			FromMethod:    route.FromMethod,
+			ToMethod:      route.ToMethod,
+			CustomHeaders: route.CustomHeaders,
+		}
+		http.Handle(route.URI, Handler(handler))
+	}
 	http.HandleFunc("/", p.ProxyRequest)
-	log.Println("Started GO proxyserver on port", p.ListeningPort)
 
+	log.Println("Starting GO proxyserver on port", p.ListeningPort)
 	err := http.ListenAndServe("127.0.0.1:"+p.ListeningPort, nil)
 	if err != nil {
 		return err
@@ -29,12 +48,15 @@ func StartProxy(p *Proxy) error {
 	return nil
 }
 
+func (mh MethodHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
+	log.Println("FromMethod", mh.FromMethod)
+	log.Println("ToMethod", mh.ToMethod)
+}
+
 func (p *Proxy) ProxyRequest(w http.ResponseWriter, r *http.Request) {
 
 	uri := p.TargetUrl + r.RequestURI
 	log.Println(r.Method + ": " + uri)
-
-	p.MethodHandler(r)
 
 	remote_request, err := p.CreateRemoteRequest(r, uri)
 	if err != nil {
@@ -62,12 +84,6 @@ func (p *Proxy) ProxyRequest(w http.ResponseWriter, r *http.Request) {
 	w.Write(body)
 }
 
-func (p *Proxy) MethodHandler(r *http.Request) {
-	if r.Method == "POST" {
-		log.Printf("Method is POST:")
-	}
-}
-
 func (p *Proxy) CreateRemoteRequest(r *http.Request, uri string) (*http.Request, error) {
 	rr, err := http.NewRequest(r.Method, uri, r.Body)
 	if err != nil {
@@ -83,7 +99,6 @@ func (p *Proxy) Query(r *http.Request) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return resp, nil
 }
 
@@ -92,7 +107,6 @@ func (p *Proxy) ReadBody(r *http.Response) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return body, nil
 }
 
